5ecbd62175b478288fdc90432944250d0f315a65
Sanchit Chadha <schadha@vt.edu>
2014-12-10 20:46:38 -0500

    Current Location Added

--- build/generated-sources/ap-source-output/com/group2/vtdt/Newsfeed_.java
+++ build/generated-sources/ap-source-output/com/group2/vtdt/Newsfeed_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-12-10T17:12:18")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-12-10T20:39:35")
 @StaticMetamodel(Newsfeed.class)
 public class Newsfeed_ { 
 
